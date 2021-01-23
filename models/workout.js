e741e5b384a631afd5717ec4889807834267c6f1
Teshera Lee <tcasey0825@gmail.com>
2021-01-23 17:44:33 -0500

    updated api
    added api's

---
+++ models/workout.js
@@ -0,0 +1,28 @@
+import mongoose, { model } from 'mongoose';
+const { Schema } = mongoose;
+
+const workoutSchema = new Schema({
+  
+  date: { type: Date, default: Date.now },
+  exercises: Array,
+
+},{
+    toObject:{
+        virtuals: true
+    },
+    toJSON:{
+        virtuals: true
+    }
+});
+
+workoutSchema.virtual('totalDuration').get(function(){
+    let total=0
+    for (let i=0; i <  this.exercises.length; i++) {
+        console.log(this.exercises[i])
+        total=this.exercises[i].duration+total
+    }
+    return total
+})
+
+const Workout=mongoose.model("Workout", workoutSchema)
+model.exports=Workout
