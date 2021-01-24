70a3038d9838046f111b54813978f489fabaf5b7
Teshera Lee <tcasey0825@gmail.com>
2021-01-24 14:29:43 -0500

    added api routes
    added api routes

---
+++ routes/api.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const db = require("models");
+let totalDur;
+router.post("/api/workouts", ({ body }, res) => {
+    totalDur = 0;
+    db.Workout.create({ body })
+        .then(dbFitness => {
+    console.log(body);
+            res.json(dbFitness)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+router.put("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+    let data = req.body;
+    totalDur+= data.duration
+    db.Workout.update({_id: id}, {$push: {exercises: data}, totalDuration: totalDur}).then(dbUpdate => {
+        res.send(dbUpdate);
+    }).catch(err => {
+        console.log(err.message)
+    })
+})
+router.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+    .then(data => {
+        res.json(data);
+    }).catch(err => {
+        console.log(err.message);
+    })
+});
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(data => {
+        res.json(data);
+    }).catch(err => {
+        console.log(err.message);
+    })
+})
+module.exports = router;
+
+
+
+
+
+
+
+
